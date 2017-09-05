b3ff4d6b8a0d4c12010ddb0ca0322cb0d046e5d9
LiuRoy <lrysjtu@gmail.com>
2017-09-05 20:37:03 +0800

    初始化项目

--- index.android.js
+++ index.android.js
@@ -1,53 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-export default class KakaSearch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+class HomeScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            splashed: false,
+        };
+    };
+
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+
+    componentDidMount() {
+        this.timer = setTimeout(
+            () => {
+                this.setState({splashed: true})
+            },
+            2000,
+        );
+    };
+
+    componentWillUnmount() {
+        this.timer && clearTimeout(this.timer);
+    }
+
+    render() {
+        if (this.state.splashed) {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.instructions}>
+                        home screen
+                    </Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.instructions}>
+                        splash screen
+                    </Text>
+                </View>
+            );
+        }
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+const KakaSearch = StackNavigator({
+    Home: {screen: HomeScreen},
 });
 
 AppRegistry.registerComponent('KakaSearch', () => KakaSearch);
