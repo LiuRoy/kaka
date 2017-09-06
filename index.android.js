fc2f6e12f862e227773558f882be38bc841edf63
LiuRoy <lrysjtu@gmail.com>
2017-09-06 14:38:58 +0800

    添加navigator

--- index.android.js
+++ index.android.js
@@ -1,57 +1,21 @@
 import React, {Component} from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 
 
 class HomeScreen extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            splashed: false,
-        };
-    };
-
     static navigationOptions = {
         title: 'Welcome',
     };
 
-    componentDidMount() {
-        this.timer = setTimeout(
-            () => {
-                this.setState({splashed: true})
-            },
-            2000,
-        );
-    };
-
-    componentWillUnmount() {
-        this.timer && clearTimeout(this.timer);
-    }
-
     render() {
-        if (this.state.splashed) {
-            return (
-                <View style={styles.container}>
-                    <Text style={styles.instructions}>
-                        home screen
-                    </Text>
-                </View>
-            );
-        } else {
-            return (
-                <View style={styles.container}>
-                    <Text style={styles.instructions}>
-                        splash screen
-                    </Text>
-                </View>
-            );
-        }
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instructions}>
+                    home screen
+                </Text>
+            </View>
+        );
     }
 }
 
